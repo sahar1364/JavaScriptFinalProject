8a01ac416589f1b975aaf1649aa06269b01f8e5a
Sahar <sahar@stratahealth.com>
2015-07-22 14:47:09 -0600

    SJ: add some comments and make some improvements to the code

--- final.js
+++ final.js
@@ -20,17 +20,28 @@ var Game = (function () {
         startGame();
     }
     
+    /*
+        Starts the game by creating all the Memory objects, showing the values for a few seconds and then hiding them from the user
+    */
     function startGame() {
         numberArray = [];
         for (var i=0; i < level+1; i++) {
             numberArray.push(new MemoryNumber(i+1));
-            var pos = generatePosition();
-            numberArray[i].setPosition(pos.x, pos.y);
+            try {
+                var pos = generatePosition();
+                numberArray[i].setPosition(pos.x, pos.y);
+            } catch(e) {
+                alert("A technical error happend, please refresh the page");
+            }
         }
         setTimeout(makeAllVisible, 3000);
         setTimeout(makeAllHidden, 3000 + level*1000);    
     }
     
+    /*
+        creates all the elements for the DOM which represent the game
+        @return: the main div for the game (gameDiv)
+    */
     function createGameDiv() {      
         quitButton = document.createElement("input");
         quitButton.setAttribute("type", "button");
@@ -59,12 +70,19 @@ var Game = (function () {
         return div;
     }
     
+    /*
+      Removes all the elements related to the game from the DOM  
+    */
     function removeCurrentLevelElementsFromPage() {
         document.body.removeChild(gameDiv);
         document.body.removeChild(quitButton);
         document.body.removeChild(levelDiv);        
     }
     
+    /*
+        Checks the game state when a click happens to see whether the game is finished or not
+        and goes to next level if the game is finished
+    */
     function checkGameState() {
         var isGameFinished = true;
         var orderIsCorrect = false;
@@ -73,12 +91,13 @@ var Game = (function () {
                 isGameFinished = false;
             }
             else if (numberArray[i].getIsClicked() && isOutOfOrder(i)) {
-                numberArray[i].gotItWrong();   
+                numberArray[i].gotItWrong(numberArray[i].getDiv());   
             }
         }
         if (isGameFinished) {
             setTimeout(goToNextLevel, 3000);
         }
+        //checks if the iVar th element in the numberArray has been clicked out of order
         function isOutOfOrder(iVar) {
             for (var j = 0; j < iVar; j++) {
                 if (!numberArray[j].getIsClicked()) {
@@ -89,10 +108,14 @@ var Game = (function () {
         }
     }
     
+    /*
+        Takes you to next level if you got all the numbers right in order, otherwise restarts the same level
+    */
     function goToNextLevel() {
         if (canGoToNextlevel()) {
             level = level + 1;
         }
+        
         removeCurrentLevelElementsFromPage();
        
         gameDiv = createGameDiv();
@@ -100,7 +123,7 @@ var Game = (function () {
         
         function canGoToNextlevel() {
             for (var i = 0; i < numberArray.length; i++) {
-                if (!numberArray[i].getClickedOnTime()) {
+                if (!numberArray[i].clickedOnTime) {
                     return false;
                 }
             }
@@ -108,55 +131,57 @@ var Game = (function () {
         }
     }
     
+    /*
+        Makes all the numbers visible on the page
+    */
     function makeAllVisible() {
         for (var i = 0; i < numberArray.length; i++) {
-            numberArray[i].setVisible();
+            numberArray[i].setVisible(numberArray[i].getDiv());
         }
     }
     
+    /*
+        Hides all the numbers from the user on the page
+    */
     function makeAllHidden() {
          for (var i = 0; i < numberArray.length; i++) {
-            numberArray[i].setHidden();
+            numberArray[i].setHidden(numberArray[i].getDiv());
         }       
     }
     
+    /*
+        Each number is an object on the div which can be constructed by passing a value in
+    */
     function MemoryNumber(val) {
+        //private variables
         var x  = 0;
         var y = 0;
         var value = val;
         var div;
-        var clickedOnTime = true;
         var isClicked = false;
         
-        this.getClickedOnTime = function() {
-            return clickedOnTime;
-        };
+        //public variables and method
+        this.clickedOnTime = true;
+
         this.getIsClicked = function() {
             return isClicked;   
         };
         this.setPosition = function(xPos, yPos) {
-            x = xPos;
-            y = yPos;
-            createDiv();
+            if (!isNaN(xPos) && !isNaN(yPos) && xPos <= GAME_DIV_WIDTH && yPos <= GAME_DIV_HEIGHT) {
+                x = xPos;
+                y = yPos;
+                createDiv();
+            }
+            else {
+                throw new Error("Generated random numbers are not correct");
+            }
         };
         this.getValue = function() {
             return value;
         };
-        this.gotItRight = function () {
-            div.setAttribute("class", "number visible right-guess");
-            clickedOnTime = true;
-        };
-        this.setVisible = function () {
-            div.setAttribute("class","number visible"); 
-        };
-        this.setHidden = function () {
-            div.setAttribute("class","number hidden"); 
-        };
-        this.gotItWrong = function () {
-            div.setAttribute("class", "number visible wrong-guess");
-            clickedOnTime = false;
-        };
-        
+        this.getDiv = function() {
+            return div;
+        }
         function createDiv() {
             div = document.createElement("div");
             div.setAttribute("class", "number hidden");
@@ -169,12 +194,27 @@ var Game = (function () {
                 isClicked = true;
             };
         }
-        function destroyDiv() {
-            document.removeChild(div);    
-        }
     }
+    
+    //set some common methods on protoptype to avoid extra memory usage
+    MemoryNumber.prototype.gotItWrong = function(div) {
+        div.setAttribute("class", "number visible wrong-guess");
+        this.clickedOnTime = false;    
+    }
+    MemoryNumber.prototype.gotItRight = function (div) {
+        div.setAttribute("class", "number visible right-guess");
+        this.clickedOnTime = true;
+    };
+    MemoryNumber.prototype.setVisible = function (div) {
+        div.setAttribute("class","number visible"); 
+    };
+    MemoryNumber.prototype.setHidden = function (div) {
+        div.setAttribute("class","number hidden"); 
+    };
+    
     /*
       Generates a random position on the plane
+      @return: an object containing x and y positions
     */
     function generatePosition() {
         var x = Math.floor(Math.random()*GAME_DIV_WIDTH);
@@ -187,8 +227,12 @@ var Game = (function () {
         construct();
     }
     
-    /* uses closure for the variables that are not in the scope and returns a public API*/
+    /* uses closure for the variables that are not in the scope and returns a public API for use*/
     return {
+        /*
+          @input: the id of the div for the start page
+          @input: the id of the start button
+        */
         start: function(div1, startId) {
             var play = document.getElementById(startId);
             play.onclick = function() {
